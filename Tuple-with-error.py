d9dec6ffd11876dae51cc323ca65eea9d4351093
ranakaplan <153181993+ranakaplann@users.noreply.github.com>
2025-11-24 22:53:10 +0300

    Add tuple with an error example
    Attempt to modify a tuple, which is immutable.

---
+++ Tuple-with-error.py
@@ -0,0 +1,5 @@
+#Tuple Challenge
+locations = ('Bulgurlu', 'Üsküdar', 'Istanbul', 'Türkiye')
+type(locations)
+print('Change the city name Istanbul to Ankara:')
+locations[2] = 'Ankara'   #this is the line we're going to get error because tuples can't be changed
