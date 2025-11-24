70105fee218878185a336cce5607ad256b2f00b9
ranakaplan <153181993+ranakaplann@users.noreply.github.com>
2025-11-24 22:52:40 +0300

    Add personal info dictionary in Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+#Personal Info Dictionary
+myDictionary = {
+    'name': 'Rana',
+    'age': 20,
+    'country': 'Turkey',
+    'favorite color': 'navy blue',
+    'favorite drink': 'tea',
+    'favorite show': 'The Office'
+}
+
+print(myDictionary)
+print(type(myDictionary))
