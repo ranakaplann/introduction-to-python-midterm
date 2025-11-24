c1e3da6e6c3c6aa291f1d19b1103cba116e25bb4
ranakaplan <153181993+ranakaplann@users.noreply.github.com>
2025-11-24 22:55:33 +0300

    Add Day of the Week Checker

---
+++ DayWeek.py
@@ -0,0 +1,17 @@
+#Day of the Week Checker
+number = int(input("Enter a number between 1 and 7: "))
+match number:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
