9e82a3df7cb7294653dc9a7e3d2216973ad35922
ranakaplan <153181993+ranakaplann@users.noreply.github.com>
2025-11-24 22:50:26 +0300

    Add DataTypeDetector.py for type detection

---
+++ DataTypeDetector.py
@@ -0,0 +1,11 @@
+#Data Type Detector
+name = 'Rana' #string
+age = 20      #integer
+isStudent = True  #boolean
+gpa = 3.28        #float
+
+
+print(type(name))
+print(type(age))
+print(type(isStudent))
+print(type(gpa))
