a493b56a2822c85f5727f1e5393d4c708b9bae7e
ranakaplan <153181993+ranakaplann@users.noreply.github.com>
2025-11-24 22:51:48 +0300

    Implement data swapping in DataSwappingChallenge.py

---
+++ DataSwappingChallenge.py
@@ -0,0 +1,8 @@
+#Data Swapping Challenge
+a = 15
+b = 25
+print("Value of a before swap:", a)
+print("Value of b before swap:", b)
+a , b = b , a
+print ("New value of a:", a)
+print ("New value of b:", b)
